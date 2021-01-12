93301678acab6aa56d50d5fe42a5a2f5098c98d4
MbZ <amobarez00@gmail.com>
2021-01-12 16:22:38 +0330

    website template 1

--- js/navbar-scroll.js
+++ js/navbar-scroll.js
@@ -1,10 +1,15 @@
-var myNav = document.getElementById('main-navbar');
+let myNav = document.getElementById('main-navbar');
+let dropDownMenu = document.getElementById('dropdown-menu');
+let dropDownItems = document.getElementsByClassName("dropdown-item");
 window.onscroll = function () { 
-    if (document.body.scrollTop >= 200 ) {
+    console.log(dropDownMenu);
+    if (window.pageYOffset >= 100 ) {
         myNav.classList.add("nav-colored");
+        dropDownMenu.classList.add("nav-colored");
     } 
     else {
         myNav.classList.remove("nav-colored");
+        dropDownMenu.classList.remove("nav-colored");
     }
 };
 
